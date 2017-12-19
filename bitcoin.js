dc2c7f95552b9fdb2f05a499731ba653b357f799
ramirobg94 <ramirobg94@gmail.com>
2017-12-19 11:42:18 +0100

    release

--- bitcoin.js
+++ bitcoin.js
@@ -1,6 +1,55 @@
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 
+
+
+
+
+
+
+
+
+
+//Fastest (forEach in spite of old for is faster)
+export const dinamicMax = (arr) => {
+	if( isEmpty(arr) ) return -1;
+
+	let maxRange = -1;
+	let minValue = arr[0];
+	arr.forEach((currentValue) => {
+		if(currentValue < minValue) minValue = currentValue;
+		if(maxRange < (currentValue - minValue )) maxRange = currentValue - minValue;
+	});
+
+	return maxRange > 0 ? maxRange : -1;
+}
+
+//Elegant
+export const maximizationWithReducersShortestVersion = (arr) => isEmpty(arr) ? -1 : retunValueIfPositive(maxValueInArray(getDiferencesToTheRight(arr)) );
+
+//Original
+export const maximizationWithReducersLongVersion = (arr) => isEmpty(arr) ? -1 : retunValueIfPositive(arr.reduce((p,cV,idx,arr) => p.concat(  Math.max(...[ ...arr.slice(idx)].reverse())-arr.slice(idx)[0]), new Array()).reduce((a,b)=>Math.max(a, b)));
+
+//Original
+export const maximizationOneLine = (arr) => {
+	let max = -1, err = false;
+	arr.forEach((currentValue, idx, ar) => {
+		if(typeof currentValue !== 'number') { err = true; }
+		if(Math.max(...[ ...ar.slice(idx)].reverse()) - currentValue >= max) { max = Math.max(...[ ...ar.slice(idx)].reverse()) - currentValue; }
+	});
+	return max > 0 && !err ? max : -1;
+}
+
+
+
+
+
+
+
+
+
+
+
 export const maxValueRight = (arr) => Math.max(...[ ...arr].reverse());
 //export const maxValueRightWithIndexOf = (arr) => arr[arr.lastIndexOf(Math.max(...arr))];
 export const getMaxDiference = (sliced) => maxValueRight(sliced) - sliced[0];
@@ -16,15 +65,6 @@ export const maximizationHuman = (arr) => {
 		max = getMax(max, getMaxDiferenceToRight(ar,idx))
 	});
 	return hasProfit(max) && !err ? max : -1;
-}
-
-export const maximizationOneLine = (arr) => {
-	let max = -1, err = false;
-	arr.forEach((currentValue, idx, ar) => {
-		if(typeof currentValue !== 'number') { err = true; }
-		if(Math.max(...[ ...ar.slice(idx)].reverse()) - currentValue >= max) { max = Math.max(...[ ...ar.slice(idx)].reverse()) - currentValue; }
-	});
-	return max > 0 && !err ? max : -1;
 }
 
 export const crazy = (arr) => {
@@ -45,30 +85,15 @@ export const retunValueIfPositive = (value) => value > 0 ? value : -1;
 
 export const isEmpty = (arr) => arr.length <= 0
 
-export const maximizationWithReducersShortestVersion = (arr) => isEmpty(arr) ? -1 : retunValueIfPositive(maxValueInArray(getDiferencesToTheRight(arr)) );
-
 export const maximizationWithReducersShorterVersion = (arr) => isEmpty(arr) ? -1 :  retunValueIfPositive(Math.max(...getDiferencesToTheRight(arr)));
 
 export const maximizationWithReducersShortVersion = (arr) => isEmpty(arr) ? -1 : retunValueIfPositive(Math.max(...arr.reduce((p,cV,idx,arr) => p.concat(getMaxDiferenceToRight(arr,idx)), new Array())));
 
 export const maximizationWithReducers = (arr) => isEmpty(arr) ? -1 : retunValueIfPositive(arr.reduce((p,cV,idx,arr) => p.concat(getMaxDiferenceToRight(arr,idx)), new Array()).reduce((a,b)=>Math.max(a, b)));
 
-export const maximizationWithReducersLongVersion = (arr) => isEmpty(arr) ? -1 : retunValueIfPositive(arr.reduce((p,cV,idx,arr) => p.concat(  Math.max(...[ ...arr.slice(idx)].reverse())-arr.slice(idx)[0]), new Array()).reduce((a,b)=>Math.max(a, b)));
-
 export const maximizationInOneTweet = (s) => isEmpty(s) ? -1 : retunValueIfPositive( s.reduce((p,c,i,a)=>p.concat(Math.max(...[...s.slice(idx)].reverse())-s.slice(idx)[0]),new Array()).reduce((a,b)=>Math.max(a, b)) )
 
-export const dinamicMax = (arr) => {
-	if( isEmpty(arr) ) return -1;
-
-	let maxRange = -1;
-	let minValue = arr[0];
-	arr.forEach((currentValue) => {
-		if(currentValue < minValue) minValue = currentValue;
-		if(maxRange < (currentValue - minValue )) maxRange = currentValue - minValue;
-	});
-
-	return maxRange > 0 ? maxRange : -1;
-}
+//export const dP = (ar) => { if(ar.length < 0 ) return -1; let P = -1; let m = ar[0]; ar.forEach((c)=>{ if(c < m){m = c}; if(P-(c-m)){P = c -m }; }) return P > 0 ? P : -1 }
 
 console.log(dinamicMax([45, 24, 35, 31, 40, 38, 24]))
 
